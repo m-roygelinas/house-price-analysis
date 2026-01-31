f9f9aed5ed9fbdb4d68c33459ad2edae109901e3
Michael R Gelinas <m.roygelinas@outlook.com>
2026-01-30 19:43:19 -0500

    Group by number of bedrooms and calculate average price

--- src/house_price_analysis.py
+++ src/house_price_analysis.py
@@ -6,3 +6,17 @@ from pathlib import Path
 # Get the directory of the current script, then go up one level to data
 data_path = Path(__file__).parent.parent / "data" / "house_price.csv"
 df = pd.read_csv(data_path)
+
+# -----------------------------------------------------------------------------
+# Get basic info about the dataframe
+# -----------------------------------------------------------------------------
+
+#print(df.head())
+print(df.info())
+
+#------------------------------------------------------------------------------
+# Analyze house prices by number of bedrooms
+# -----------------------------------------------------------------------------
+
+# Group by number of bedrooms and calculate average price
+df_price_bedrooms = df.groupby("bedrooms")["price"].mean()
